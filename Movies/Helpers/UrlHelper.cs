343b0b9e722bf9a62012c429c1f4813249209257
orzech <michalorzechowski123@gmail.com>
2017-11-12 23:03:10 +0100

    fixed Back to List in movies' details

---
+++ Movies/Helpers/UrlHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace Movies.Helpers
+{
+    public static class UrlHelper
+    {
+        public static string ActionRedirectToReferrer(this System.Web.Mvc.UrlHelper helper, HttpRequestBase request)
+        {
+            return request.UrlReferrer?.ToString() ?? helper.Action("Index", "Movies");
+        }
+    }
+}
